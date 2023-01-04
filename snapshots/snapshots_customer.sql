4c6fcaab45a486dac625503f6ed6407a4b8d7c63
Jayboy628 <shaunjay.brown@gmail.com>
2023-01-03 21:14:56 -0500

    slowly changeing 2 for dimcustomer 04

--- snapshots/snapshots_customer.sql
+++ snapshots/snapshots_customer.sql
@@ -1,15 +1,18 @@
 {% snapshot snapshot_customer %}
 
+{% set new_schema = target.schema + '_snapshot' %}
+
+
 {{
   config(
     target_database='ANALYTICS_DEV'
-    target_schema = 'warehouse',
+    target_schema = new_schema,
     unique_key = 'customerkey',
     strategy = 'check',
     check_cols = ['city','state_province_code', 'stateprovince', 'country_region_code', 'country', 'postal_code']
     )
 }}
 
-select * from {{ref('warehouse','dim_customer')}}
+select * from {{ref('dim_customer')}}
 
 {% endsnapshot %}
