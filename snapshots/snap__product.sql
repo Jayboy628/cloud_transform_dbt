01fca46808636e65fe6ccdea9da57a960508a8ae
Jayboy628 <shaunjay.brown@gmail.com>
2023-01-11 10:04:28 -0500

    product updates test sl2

--- snapshots/snap__product.sql
+++ snapshots/snap__product.sql
@@ -14,7 +14,7 @@
 {%- set t1_cols = ['STANDARDCOST','LISTPRICE','MODIFIEDDATE'] -%}
 
 select 
-productid::varchar || '-' || to_char(convert_timezone( 'America/New_York',sysdate::timestamp),'YYYYMMDDHH24MISS') as PRODUCT_KEY,
+productid::varchar || '-' || to_char(convert_timezone( 'America/New_York',current_timestamp::timestamp),'YYYYMMDDHH24MISS') as PRODUCT_KEY,
 *,
         {{ dbt_utils.generate_surrogate_key(t1_cols) }} as t1_KEY
        
